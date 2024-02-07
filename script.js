055c89fb36060a77c3feeeac99c1b8ff1bc473ea
Raul Vinicius <raullwx@gmail.com>
2024-02-07 13:53:26 -0400

    new feature: input validate

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-const inputElement = document.querySelector('.new-task-input');
-const addTaskBtn = document.querySelector('.new-task-btn');
+const inputElement = document.querySelector(".new-task-input");
+const addTaskBtn = document.querySelector(".new-task-btn");
 
 const validadeInput = () => {
     return inputElement.value.trim().length > 0;
 }
 
+const handleAddTask = () => {
+    const inputIsValid = validadeInput();
+
+    if (!inputIsValid) {
+        return inputElement.classList.add("error");
+    }
+}
+
+const handleInputChange = () => {
+    const inputIsValid = validadeInput();
+
+    if (inputIsValid) {
+        return inputElement.classList.remove("error");
+    }
+}
+
+addTaskBtn.addEventListener("click", () => handleAddTask());
+inputElement.addEventListener("change", () => handleInputChange());
