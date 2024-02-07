6176c4f65ff61cb37c9060c3f1c19479026aa072
Raul Vinicius <raullwx@gmail.com>
2024-02-07 15:36:06 -0400

    add new feature: adding tasks

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const inputElement = document.querySelector(".new-task-input");
 const addTaskBtn = document.querySelector(".new-task-btn");
+const taskContainer = document.querySelector(".task-container");
 
 const validadeInput = () => {
     return inputElement.value.trim().length > 0;
@@ -11,6 +12,26 @@ const handleAddTask = () => {
     if (!inputIsValid) {
         return inputElement.classList.add("error");
     }
+
+    const taskItemContainer = document.createElement("div");
+    taskItemContainer.classList.add("task-item");
+
+    const taskItemContent = document.createElement("p");
+    taskItemContent.innerText = inputElement.value;
+
+    const taskItemDelete = document.createElement("i");
+    taskItemDelete.classList.add("fa-solid");
+    taskItemDelete.classList.add("fa-trash");
+
+    taskItemContainer.appendChild(taskItemContent);
+    taskItemContainer.appendChild(taskItemDelete);
+
+    taskContainer.classList.remove("hidden-task-container");
+    taskContainer.classList.add("show-task-container");
+
+    taskContainer.appendChild(taskItemContainer);
+
+    inputElement.value = '';
 }
 
 const handleInputChange = () => {
@@ -19,6 +40,7 @@ const handleInputChange = () => {
     if (inputIsValid) {
         return inputElement.classList.remove("error");
     }
+
 }
 
 addTaskBtn.addEventListener("click", () => handleAddTask());
